c332e907de34c7f14b5b9a8fd71e99fbc65879a0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-22 18:44:57 +0100

    Create Hero.tsx

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Hero = () => {
+    return (
+        <div className='w-full  bg-[url("/background/chateau-dessin.png")] bg-cover bg-center'>
+            <div className='mx-auto flex max-w-5xl py-2 px-4 justify-center items-center min-h-[392px] w-full'>
+                <div className='flex flex-col items-center'>
+                    <Image src='/logo/logo-hero.png' alt='Château Dutruch Grand Poujeaux' width={300} height={200} />
+                    <div className="mt-10 flex items-end">
+                        <div className='w-14 h-0.5 bg-red mb-1'></div>
+                        <Image src='/logo/logo-moulis-medoc.png' alt='moulis-en-medoc' width={150} height={50} className='mr-3 ml-3' />
+                        <div className='w-14 h-0.5 bg-red mb-1'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
