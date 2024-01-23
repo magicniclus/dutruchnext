84c56d188556a21b451505b6183d9a7da8b15698
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-23 14:20:22 +0100

    Create language.ts

---
+++ src/lib/language.ts
@@ -0,0 +1,8 @@
+export const language: Record<'fr' | 'eng', { title: string }> = {
+    fr : {
+        "title": "Bonjour et bienvenue sur mon site",
+    },
+    eng : {
+        "title": "Hello and welcome on my website",
+    }
+};
