615f1298dfc83de6f75527be36760d379e974ac2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-23 18:57:29 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,29 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Who from "./components/Who";
+import Lightbox from "./components/Lightbox";
+
+import { RootState } from '@/redux/store';
+
+import { useSelector } from 'react-redux';
 
 import Image from "next/image";
 
 export default function Home() {
 
+    // Sélectionnez la clé de langue actuelle de l'état Redux
+    type LanguageKey = 'fr' | 'eng';
+    const languageKey = useSelector((state: RootState) => state.language.language) as LanguageKey;
+
   return (
     <>
         <header>
           <Nav />
-          <Hero />
-          <Who />
         </header>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="">
+          <Hero />
+          <Who lang={languageKey} />
+          <Lightbox />
         </main>
     </>
   );
