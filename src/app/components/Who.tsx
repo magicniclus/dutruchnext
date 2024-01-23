9dc2e233b22327d77d016eebdaa0ca395f1fa8dd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-23 14:38:50 +0100

    Update Who.tsx

--- src/app/components/Who.tsx
+++ src/app/components/Who.tsx
@@ -1,7 +1,25 @@
+import React, { useState, useEffect } from 'react';
 
 const Who = () => {
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowBackground(window.innerWidth < 1440);
+    };
+
+    // Définir l'état initial en fonction de la largeur de la fenêtre
+    handleResize();
+
+    // Ajouter le gestionnaire d'événements pour le redimensionnement
+    window.addEventListener('resize', handleResize);
+
+    // Nettoyer le gestionnaire d'événements lors du démontage
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className='w-full  bg-[url("/background/vigne.png")] bg-cover bg-right min-h-[613px]'>
+    <div className={`w-full bg-cover bg-right min-h-[613px] ${showBackground ? 'md:bg-[url("/background/vigne.png")]' : ''}`}>
       
     </div>
   );
