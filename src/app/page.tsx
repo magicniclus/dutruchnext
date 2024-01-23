3b452257704100ea8acfc5cdaf1886a6f1b4b270
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-23 14:20:14 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,21 @@
 
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import Who from "./components/Who";
 
 import Image from "next/image";
 
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-
 export default function Home() {
+
   return (
-    
-      <Provider store={store}>
+    <>
         <header>
           <Nav />
           <Hero />
+          <Who />
         </header>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
         </main>
-      </Provider>
+    </>
   );
 }
