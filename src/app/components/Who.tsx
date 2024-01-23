2fa5c6dd0ce3f7dbe0764bde970bc9319dd3168e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-23 14:20:11 +0100

    Create Who.tsx

---
+++ src/app/components/Who.tsx
@@ -0,0 +1,10 @@
+
+const Who = () => {
+  return (
+    <div className='w-full  bg-[url("/background/vigne.png")] bg-cover bg-right min-h-[613px]'>
+      
+    </div>
+  );
+}
+
+export default Who;
