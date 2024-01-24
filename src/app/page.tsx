6e2921c0b85db1577a0bfec17ff8b330657294c8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-24 11:18:33 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,28 @@ import { RootState } from '@/redux/store';
 import { useSelector } from 'react-redux';
 
 import Image from "next/image";
+import { useState, useEffect } from "react";
+
+import Descritption from "./components/Descritpion";
 
 export default function Home() {
+    
+    const [showBackground, setShowBackground] = useState(false);
+      
+    useEffect(() => {
+      const handleResize = () => {
+      setShowBackground(window.innerWidth < 1440);
+    };
+
+    // Définir l'état initial en fonction de la largeur de la fenêtre
+    handleResize();
+
+    // Ajouter le gestionnaire d'événements pour le redimensionnement
+    window.addEventListener('resize', handleResize);
+
+    // Nettoyer le gestionnaire d'événements lors du démontage
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
     // Sélectionnez la clé de langue actuelle de l'état Redux
     type LanguageKey = 'fr' | 'eng';
@@ -26,6 +46,9 @@ export default function Home() {
           <Hero />
           <Who lang={languageKey} />
           <Lightbox />
+          <div className={`w-full bg-cover bg-right py-20 ${showBackground ? 'bg-[url("/background/dessin-marie.png")]' : ''}`}>
+            <Descritption lang={languageKey} />
+          </div>
         </main>
     </>
   );
