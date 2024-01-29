888089a2b23b781512c70d04b41e13dab9d53569
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-29 11:35:20 +0100

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className='w-full z-100 flex flex-col'>   
             <div className=' bg-text py-10'>
-                <div className='mx-auto flex max-w-5xl px-4 py-2 justify-between items-stretch w-full md:flex-row flex-col'>
+                <div className='mx-auto flex max-w-7xl px-4 py-2 justify-between items-stretch w-full md:flex-row flex-col'>
                     <div className='flex flex-col items-center'>
                         <img src="/logo/logo-footer.png" className='max-w-[244px]' />
                         <img src="/logo/logo-moulis-medoc-footer.png" className='max-w-[140px] mt-7' />
@@ -25,7 +25,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className=' py-7 px-4 bg-white mx-auto flex md:flex-row flex-col max-w-5xl justify-between w-full'>
+            <div className=' py-7 px-4 bg-white mx-auto flex md:flex-row flex-col max-w-7xl justify-between w-full'>
                 <div className='flex'>
                     <img src="/logo/exploitation.png" className='max-w-[244px] mx-auto' />
                     <img src="/logo/independant.png" className='max-w-[244px] mx-auto ml-3' />
