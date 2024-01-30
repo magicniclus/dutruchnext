983db2fdbbd096a778c2f07de9bba1a2ab730035
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-30 10:00:23 +0100

    Update Descritpion.tsx

--- src/app/components/Descritpion.tsx
+++ src/app/components/Descritpion.tsx
@@ -35,71 +35,6 @@ const Descritpion = ({ lang }: DescriptionProps) => {
     const paraphRefTwo = useRef(null);
     const logoRef = useRef(null);
 
-    // useEffect(() => {
-    //     //Timeline
-    //     const tl = gsap.timeline({
-    //         scrollTrigger: {
-    //         trigger: titleRef.current, // Vous pourriez vouloir ajuster ceci selon le meilleur élément déclencheur
-    //         start: 'top 70%', // Ajustez selon vos besoins
-    //         toggleActions: 'play none none none',
-    //         immediateRender: true,
-    //         // markers: true
-    //         }
-    //     });
-
-    //     // Ajouter des animations à la timeline
-    //     tl.fromTo(backgroundRef.current, {opacity: 0}, {opacity: 1, duration: 0.2})
-    //     tl.fromTo(titleRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0)
-    //     .fromTo(subtitleRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.2) 
-    //     .fromTo(titleRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0)
-    //     .fromTo(subtitleRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.2)
-    //     .fromTo(lignRef.current, { x: -100 }, { x: 0, duration: 0.7 }, 0.3)
-    //     .fromTo(lignRefTwo.current, { x: -100 }, { x: 0, duration: 0.7 }, 0.3)
-    //             .fromTo(lignTextRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3)
-    //     .fromTo(lignTextRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.45s
-    //     .fromTo(lignTextRefThree.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.5s
-    //     .fromTo(lignTextRefFour.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.55s
-    //     .fromTo(lignTextRefFive.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.6s
-    //     .fromTo(lignTextRefSix.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.65s
-    //     .fromTo(lignTextRefSeven.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.7s
-    //     .fromTo(lignTextRefEight.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.75s
-    //     .fromTo(lignTextRefNine.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.8s
-    //     .fromTo(paraphRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.4)
-    //     .fromTo(paraphRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.6)
-    // }, []);
-
-    //  useEffect(() => {
-    //     //Timeline
-    //     const tl = gsap.timeline({
-    //         scrollTrigger: {
-    //         trigger: titleRef.current, // Vous pourriez vouloir ajuster ceci selon le meilleur élément déclencheur
-    //         start: 'top 80%', // Ajustez selon vos besoins
-    //         toggleActions: 'play none none none',
-    //         // markers: true
-    //         }
-    //     });
-
-    //     // Ajouter des animations à la timeline
-    //     tl.fromTo(backgroundRef.current, {opacity: 0}, {opacity: 1, duration: 0.2})
-    //     tl.fromTo(titleRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0)
-    //     .fromTo(subtitleRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.2) 
-    //     .fromTo(titleRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0)
-    //     .fromTo(subtitleRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.2)
-    //     .fromTo(lignRef.current, { x: -100 }, { x: 0, duration: 0.7 }, 0.3)
-    //     .fromTo(lignRefTwo.current, { x: -100 }, { x: 0, duration: 0.7 }, 0.3)
-    //     .fromTo(lignTextRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3)
-    //     .fromTo(lignTextRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.45s
-    //     .fromTo(lignTextRefThree.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.5s
-    //     .fromTo(lignTextRefFour.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.55s
-    //     .fromTo(lignTextRefFive.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.6s
-    //     .fromTo(lignTextRefSix.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.65s
-    //     .fromTo(lignTextRefSeven.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.7s
-    //     .fromTo(lignTextRefEight.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.75s
-    //     .fromTo(lignTextRefNine.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.3) // Commence à 0.8s
-    //     .fromTo(paraphRef.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.4)
-    //     .fromTo(paraphRefTwo.current, { opacity: 0 }, { opacity: 1, duration: 0.7 }, 0.6)
-    // }, [lang]);
-
     const textFr = () => {
         return(
             <div className='w-full flex justify-between font-bebas md:flex-row flex-col'>
@@ -147,9 +82,47 @@ const Descritpion = ({ lang }: DescriptionProps) => {
 
     const textAng = () => {
         return(
-            <>
-                <div></div>
-            </>
+            <div className='w-full flex justify-between font-bebas md:flex-row flex-col'>
+                <div className='bg-white p-7 md:w-[48%] w-full'>
+                    <h2 className='uppercase text-red font-bebas text-[36px] ' ref={titleRef}>Château dutruch grand poujeaux</h2>
+                    <h3 className='uppercase font-bebas text-[24px] text-text ' ref={subtitleRef}>FROM FAMILY TO FAMILY</h3>
+                    <div className='overflow-hidden'>
+                        <div className="w-[84px] h-0.5 bg-red mt-4" ref={lignRef}></div>
+                    </div>
+                    <ul className="mt-10 font-cormorant text-[19px]">
+                        <li ref={lignTextRef} className='flex'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>XIXe</span>The creation of the property by the Dutruch family.</li>
+                        <li ref={lignTextRefTwo} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>1868</span>The first traces of Château Dutruch Grand Poujeaux can be found in the Féret guide</li>
+                        <li ref={lignTextRefThree} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>1932</span>Cru Bourgeois Supérieur classification.</li>
+                        <li ref={lignTextRefFour} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>1966</span>Cru Bourgeois Exceptionnel classification.</li>
+                        <li ref={lignTextRefFive} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>1967</span>François Cordonnier (senior), former manager of the Château de Chenonceau in the Loire region buys the property from the descendants of the Dutruchs. He carefully developed the vineyard from 11 to 28 hectares</li>
+                        <li ref={lignTextRefSix} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>1978</span>Cru Bourgeois Exceptionnel classification.</li>
+                        <li ref={lignTextRefSeven} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>1994</span>Although childless himself, François passes the property on to his nephew Jean Baptiste</li>
+                        <li ref={lignTextRefEight} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>2016</span>François Cordonnier (junior) takes over the chateau, continuing his uncle’s work, a perpetual striving to improve quality.</li>
+                        <li ref={lignTextRefNine} className='flex mt-1'><span className="mr-2 min-w-4 h-0.5 bg-red mt-3 "></span><span className='text-red mr-3'>2018</span>First block organically farmed, HVE3 certification </li>
+                    </ul>
+                </div>
+                <div className='bg-white p-7 md:w-[48%] w-full md:mt-0 mt-5'>
+                    <h2 className='uppercase text-red font-bebas  text-[36px] ' ref={titleRefTwo}>PRESERVING THE ENVIRONMENT</h2>
+                    <h3 className='uppercase font-bebas  text-[24px] text-text ' ref={subtitleRefTwo}>TO ATTAIN EXCELLENCE</h3>
+                    <div className='overflow-hidden'>
+                        <div className="w-[84px] h-0.5 bg-red mt-4" ref={lignRefTwo}></div>
+                    </div>
+                    <p className='mt-10 font-cormorant text-[19px]' ref={paraphRef}>
+                        If the notion of respect for the terroir is often associated with environmental measures today, we are attached to another important dimension, which is the emphasis on typicity: finesse and elegance.
+                        The perpetual striving for quality obliges us to surpass ourselves in order to achieve the best out of each vintage. Over the years, our team has acquired know-how and an exact knowledge of our vineyards, enabling the production of great wines. Since 2016 this ongoing adventure has seen us improve and hone our environmental methods so that the only traces we leave are those of impeccable quality.
+                    </p>
+                    <div className='flex justify-between font-cormorant mt-7 w-full'>
+                        <div className='w-[72%]'>
+                            <p ref={paraphRefTwo} className='text-[19px]'>
+                                We have been certified High Environmental Value level 3 since the 2018 vintage and have been trialing organic farming on a plot of 3.2 hectares since the same year.
+                            </p>
+                        </div>
+                        <div ref={logoRef}>
+                            <Image src='/logo/logo-HVE.png' alt="HVE" width={120} height={100} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
     
