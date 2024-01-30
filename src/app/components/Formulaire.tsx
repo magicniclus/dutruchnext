21d819b35fe8fc7691cce82ff81bd57580ce3c14
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-01-30 10:00:25 +0100

    Update Formulaire.tsx

--- src/app/components/Formulaire.tsx
+++ src/app/components/Formulaire.tsx
@@ -107,7 +107,7 @@ interface FormulaireProps {
                                     </div>
                                     <div className='flex flex-col md:ml-10 md:w-[30%] md:mt-0 mt-3'>
                                         <label className='font-cormorant ml-2 text-text' htmlFor="name">Entreprise (le cas échéant)</label>
-                                        <input type="text" name="name" id="entreprise" placeholder='CAVE & CIE' className='text-sm p-2 text-source rounded-sm' onChange={(e)=>setEntreprise(e.target.value)} value={entreptrise} />
+                                        <input type="text" name="name" id="entreprise" placeholder='CAVE & CIE' className='text-sm p-2 text-source rounded-sm' onChange={(e)=>setEntreprise(e.target.value)} value={entreprise} />
                                     </div>
                                 </div>
                             </div>
@@ -166,8 +166,8 @@ interface FormulaireProps {
                     <section className='mx-auto flex flex-col md:max-w-5xl md:px-4 w-full'>
                         <div className='bg-beige w-full md:translate md:-translate-y-16 p-7'>
                             <div>
-                                <h2 className="text-red font-bebas text-3xl">Merci de nous avoir contacté</h2>
-                                <h3 className='font-bebas text-xl text-text'>Nous reviendrons vers vous rapidement.</h3>
+                                <h2 className="text-red font-bebas text-3xl">Thank you for having contacted us</h2>
+                                <h3 className='font-bebas text-xl text-text'>We will get back to you quickly.</h3>
                                 <div className="mt-3 w-[85px] h-0.5 bg-red"></div>
                             </div>
                         </div>
@@ -178,8 +178,8 @@ interface FormulaireProps {
                               <section className='mx-auto flex flex-col md:max-w-5xl md:px-4 w-full'>
                     <div className='bg-beige w-full md:translate md:-translate-y-16 p-7'>
                         <div>
-                            <h2 className="text-red font-bebas text-3xl">NOUS CONTACTER</h2>
-                            <h3 className='font-bebas text-xl text-text'>Pour plus de renseignements</h3>
+                            <h2 className="text-red font-bebas text-3xl">CONTACT US</h2>
+                            <h3 className='font-bebas text-xl text-text'>For more information</h3>
                             <div className="mt-3 w-[85px] h-0.5 bg-red"></div>
                         </div>
                         <form onSubmit={handleSubmit}>
@@ -187,16 +187,16 @@ interface FormulaireProps {
                                 <div className='flex md:flex-row flex-col'>
                                     <div className="flex md:w-[40%] justify-between md:flex-row flex-col">
                                         <div className='flex flex-col md:mt-0'>
-                                            <label className='font-cormorant ml-2 text-text' htmlFor="name">Prenom*</label>
+                                            <label className='font-cormorant ml-2 text-text' htmlFor="name">First name*</label>
                                             <input type="text" name="name" id="name" placeholder='JHON' className='text-sm p-2 text-source rounded-sm' onChange={(e)=>setPrenom(e.target.value)} value={prenom} />
                                         </div>
                                         <div className='flex flex-col md:mt-0 mt-3'>
-                                            <label className='font-cormorant ml-2 text-text' htmlFor="name">Nom*</label>
+                                            <label className='font-cormorant ml-2 text-text' htmlFor="name">Name*</label>
                                             <input type="text" name="name" id="lastName" placeholder='DOE' className='text-sm p-2 text-source rounded-sm' onChange={(e)=>setNom(e.target.value)} value={nom} />
                                         </div>
                                     </div>
                                     <div className='flex flex-col md:ml-10 md:w-[30%] md:mt-0 mt-3'>
-                                        <label className='font-cormorant ml-2 text-text' htmlFor="name">Entreprise (le cas échéant)</label>
+                                        <label className='font-cormorant ml-2 text-text' htmlFor="name">Company (if applicable)</label>
                                         <input type="text" name="name" id="entreprise" placeholder='CAVE & CIE' className='text-sm p-2 text-source rounded-sm' onChange={(e)=>setEntreprise(e.target.value)} value={entreprise} />
                                     </div>
                                 </div>
@@ -211,11 +211,11 @@ interface FormulaireProps {
                                     </div>
                                     <div className="flex md:w-[40%] md:ml-10 md:flex-row flex-col">
                                         <div className='flex flex-col md:mt-0 mt-3'>
-                                            <label className='font-cormorant ml-2 text-text' htmlFor="phone">Téléphone*</label>
+                                            <label className='font-cormorant ml-2 text-text' htmlFor="phone">Phone*</label>
                                             <input type="number" name="phone" id="phone" placeholder='05 56 00 00 00' className='text-sm p-2 text-source rounded-sm md:max-w-[140px]' onChange={(e)=>setTelephone(e.target.value)} value={telephone} />
                                         </div>
                                         <div className='flex flex-col md:ml-10 md:mt-0 mt-3'>
-                                            <label className='font-cormorant ml-2 text-text' htmlFor="country">Pays*</label>
+                                            <label className='font-cormorant ml-2 text-text' htmlFor="country">Country*</label>
                                             <input type="text" name="country" id="pays" placeholder='FRANCE' className='text-sm p-2 text-source rounded-sm' onChange={(e)=>setPays(e.target.value)} value={pays} />
                                         </div>
                                     </div>
@@ -224,7 +224,7 @@ interface FormulaireProps {
                             <div className='mt-3'>
                                 <div className="flex w-full">
                                     <div className='flex flex-col w-full'>
-                                        <label className='font-cormorant ml-2 text-text' htmlFor="email">Précisez votre demande pour être recontacté*</label>
+                                        <label className='font-cormorant ml-2 text-text' htmlFor="email">Specify your request to be contacted*</label>
                                         <input type="text" name="text" id="text" placeholder='VOTRE MESSAGE' className='text-sm w-full p-2 text-source rounded-sm' onChange={(e)=>setMessage(e.target.value)} value={message} />
                                     </div>
                                 </div>
@@ -232,17 +232,17 @@ interface FormulaireProps {
                             <div className='mt-5 text-text text-sm flex justify-between md:flex-row flex-col'>
                                 <div className='md:w-1/2 border-none'>
                                     <input type="checkbox" name="checkbox" id="checkbox" className='mr-2' onChange={e=>setCheckbox(e.target.checked)} checked={checkbox}  />
-                                    <label className='font-cormorant text-text' htmlFor="checkbox">J’accepte que mes données personnelles soient collectées afin que CHÂTEAU DUTRUCH GRAND POUJEAUX puisse me recontacter.</label>
+                                    <label className='font-cormorant text-text' htmlFor="checkbox">I accept that my personal data will be collected so that CHÂTEAU DUTRUCH GRAND POUJEAUX can contact me again.</label>
                                 </div>
                                 <div className='md:w-[220px] flex md:items-end text-text justify-between md:flex-row flex-col md:mt-0 mt-5'>
-                                    <p className='text-xs font-cormorant md:mb-0 mb-2'>*Champs obligatoires</p>
+                                    <p className='text-xs font-cormorant md:mb-0 mb-2'>*Required fields</p>
                                     <button
                                         disabled={!isValid}
                                         type="submit"
                                         className={`justify-center flex bg-red px-5 py-2 rounded-full items-center cursor-pointer transition duration-300 ease-in-out ${!isValid ? "opacity-50" : "opacity-100 hover:shadow-md hover:scale-105"}`}
                                     >
                                         <img src="/icon/send.png" alt="send" className='mr-3 h-5' />
-                                        <p className='font-bebas bold text-white'>ENVOYER</p>
+                                        <p className='font-bebas bold text-white'>SEND</p>
                                     </button>
                                 </div>
                             </div>
