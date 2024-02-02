4638e5b97151bb2eb7418449c104604b0a9e5003
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-02 15:38:07 +0100

    Create page.tsx

---
+++ src/app/mentions-legales/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React from 'react';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
+const page = () => {
+    return (
+        <>
+            <header>
+                <Nav />
+            </header>
+            <main className=" max-w-7xl px-7 py-2 text-text font-cormorant mb-5">
+                <h1 className='text-[24px] font-bold mt-5'>Mentions légales</h1>
+                <div>
+                    <h2 className=" text-[20px] font-bold mt-5">ÉDITION</h2>
+                    <p>CHATEAU DUTRUCH GRAND POUJEAUX</p>
+                    <p>10, rue de la Forge</p>
+                    <p>33480 MOULIS-EN-MÉDOC</p>
+                    <p>Pour toute information liée à l’utilisation ou au fonctionnement du site http://www.chateaudutruch.com/ merci de contacter contact@chateaudutruch.com </p>
+                </div>
+                <h2 className=" text-[20px] font-bold mt-5">WEBDESIGN, DÉVELOPPEMENT WEB ET HÉBERGEMENT DU SITE</h2>
+                <div>
+                    <h3 className="text-[18px] font-bold mt-5">WEBDESIGN</h3>
+                    <p>Nancy MARTIN</p>
+                    <p>2 chemin de Lou Jouquet</p>
+                    <p>33610 CESTAS</p>
+                    <p>nancy.martin@hotmail.fr</p>
+                </div>
+                <div>
+                    <h3 className="text-[18px] font-bold mt-5">DEVELOPPEMENT WEB</h3>
+                    <p>Nicolas CASTERA</p>
+                    <p>22 rue de Libourne</p>
+                    <p>33100 BORDEAUX</p>
+                    <p>casteranicolas.contact@gmail.com</p>
+                </div>
+                <div>
+                    <h3 className="text-[18px] font-bold mt-5">HÉBERGEMENT DU SITE</h3>
+                    <p>02SWITCH</p>
+                    <p>https://www.o2switch.fr/ </p>
+                </div>
+                <div>
+                    <h3 className="text-[18px] font-bold mt-5">CREDITS PHOTOS ET ILLUSTRATIONS</h3>
+                    <p>Philippe Labeguerie, photographe</p>
+                    <p>Marie Panchout, Directrice Artistique</p>
+                </div>
+                <h2 className=" text-[20px] font-bold mt-5">DROITS</h2>
+                <div>
+                    <p>
+                        L’ensemble des textes, images, animations et logos qui constituent le Site internet http://www.chateaudutruch.com/ (et tous les sites rattachés à ce nom de domaine) sont la propriété exclusive du Château Dutruch Grand Poujeaux sauf autorisation de publication explicite.
+                        Les éléments présentés sur le site font l’objet d’une protection au titre du droit d’auteur et d’autres droits de propriété intellectuelle.
+                    </p>
+                    <p>Toute reproduction, traduction et/ou transformation, partielle ou intégrale, adaptation ou transfert sur un autre support ou site web sont strictement interdits, et sont susceptibles de constituer une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle.</p>
+                    <p>A titre exceptionnel, le Château Dutruch Grand Poujeaux peut autoriser expressément la reproduction exacte d’une ou de plusieurs parties du site, à des fins strictement privées. Conformément à la réglementation française (article 24 de la Loi n°78/17 du 6 janvier 1978) et internationale sur la propriété intellectuelle, toute utilisation non expressément autorisée constitue une infraction qui expose son auteur à des poursuites judiciaires.</p>
+                    <p>Les bases de données figurant sur ce Site, dont l’Editeur est producteur, sont protégées par les dispositions de la Loi 98-536 du 1er juillet 1998 portant transposition dans le Code de la propriété intellectuelle de la directive 96/9/CE du Parlement européen et du Conseil, du 11 mars 1996, concernant la protection juridique des bases de données.</p>
+                    <p>Les marques ou logos figurant sur le site, enregistrées ou non, appartenant à l’Éditeur ou à des tiers ne peuvent être utilisées sans l’autorisation écrite de leur propriétaire.</p>
+                </div>
+                <h2 className=" text-[20px] font-bold mt-5">DONNEES PERSONNELLES</h2>
+                <p>Toute personne répertoriée sur notre site dispose à tout moment d’un droit d’accès, de rectification et de suppression des informations la concernant. Merci d’adresser une demande écrite à :
+                    Château Dutruch Grand Poujeaux - 10, rue de la Forge - 33480 MOULIS-EN-MÉDOC.
+                </p>
+                <h2 className=" text-[20px] font-bold mt-5">PROTECTION ET TRAITEMENT DE DONNEES A CARACTERE PERSONNEL</h2>
+                <div>
+                    <h3 className="text-[18px] font-bold mt-5">RESPECT DE LA VIE PRIVEE </h3>
+                    <p>Les informations saisies par l’internaute dans des formulaires en ligne sur le Site pour formuler une question ou une observation, font l’objet d’un traitement automatisé. Elles peuvent être utilisées par le Château Dutruch Grand Poujeaux dans le cadre de ses opérations de promotion et de communication.</p>
+                    <p>Aucune information personnelle n’est collectée à l’insu des utilisateurs ou cédée à des tiers.</p>
+                    <h3 className="text-[18px] font-bold mt-5">DONNEES PERSONNELLES</h3>
+                    <p>Conformément aux dispositions de la Loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, les utilisateurs disposent à tout moment d’un droit d’accès, de modification, de rectification et de suppression des données personnelles qui les concernent. Pour exercer ce droit, s’adresser par courrier à Château Dutruch Grand Poujeaux - 10, rue de la Forge - 33480 MOULIS-EN-MÉDOC., ou par email à contact@chateaudutruch.com.</p>
+                    <h3 className="text-[18px] font-bold mt-5">DONNEES STATISTIQUES </h3>
+                    <p>Le Château Dutruch Grand Poujeaux se réserve le droit d’établir des données statistiques sur le nombre de visiteurs, la durée de visite, l’origine des visiteurs ou encore les pages les plus vues…</p>
+                    <p>Les informations permettant d’extraire les données statistiques sont recueillies à l’aide des données de connexion appelées cookies.</p>
+                    <h3 className="text-[18px] font-bold mt-5">UTILISATION DES COOKIES</h3>
+                    <p>Le Château Dutruch Grand Poujeaux conserve les cookies nécessaires aux extractions statistiques.</p>
+                    <p>Vous avez le droit de refuser un cookie via les configurations de votre navigateur web ou en modifiant les paramètres de cookies à l’apparition du message vous permettant de gérer ces données lors de votre première visite.</p>
+                     <h3 className="text-[18px] font-bold mt-5">RESPONSABILITE</h3>
+                    <p>Nous ne pouvons garantir l’exactitude des informations diffusées sur le site internet du Château Dutruch Grand Poujeaux, ni être tenu pour responsable des suites de leur utilisation par les internautes. L’Éditeur se réserve le droit de corriger ou de modifier le contenu du Site, à tout moment et sans préavis.<br/>
+                    L’Éditeur peut refuser l’accès au Site, ou à une partie du Site, sans préavis, en raison de problèmes techniques inattendus ou si l’Éditeur estime, à sa seule discrétion, que le comportement ou les activités d’un utilisateur constituent une violation des présentes Mentions légales.<br/>
+                    L’Éditeur ne peut pas garantir et ne garantit pas que le serveur accueillant le Site soit exempt de virus et que les fichiers accessibles par téléchargement sur le Site ou tout autre site tiers sont dépourvus de virus ou d’erreurs de fonctionnement. L’éditeur ne pourra être tenu responsable d’un dommage quelconque pouvant se produire lors de la connexion et de la navigation sur le Site.
+                    </p>
+                    <p>Le site internet du Château Dutruch Grand Poujeaux peut diffuser des liens vers d’autres sites Internet. Toutefois, il ne peut être tenu pour responsable d’informations diffusées sur ces sites ou de tout préjudice survenu des suites de l’utilisation de biens ou services mis à disposition sur ces sites.</p>
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export default page;
