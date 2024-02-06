46068c8bde09a1393f357220d250813934130c4e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-06 09:54:48 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 
-import Image from "next/image";
 import { useRouter } from 'next/navigation'
 
 import Nav from "./components/Nav";
@@ -20,12 +19,42 @@ import { setValidation } from '@/redux/validationOld';
 import Descritption from "./components/Descritpion";
 import Formulaire from "./components/Formulaire";
 
+const preloadImage = (src: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve();
+    img.onerror = (error) => reject(error);
+  });
+};
+
 export default function Home() {
 
   const dispatch = useDispatch();
 
   type validationAge = true | false | null;
   const validationAge = useSelector((state: RootState) => state.validationAge);
+
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+    const [isImagesLoaded, setIsImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    // URLs des images à charger
+    const imageSources = [
+      '/logo/logo-hero.png',
+      '/logo/logo-moulis-medoc.png',
+    ];
+
+    // Précharger toutes les images et mettre à jour l'état une fois chargées
+    Promise.all(imageSources.map(src => preloadImage(src)))
+      .then(() => {
+        // Toutes les images sont chargées
+        setImagesLoaded(2)
+      })
+      .catch((error) => {
+        console.error("Erreur lors du chargement des images", error);
+      });
+  }, []);
   
   const [showBackground, setShowBackground] = useState(false);
 
@@ -38,23 +67,14 @@ export default function Home() {
   const router = useRouter();
     
   useEffect(() => {
-    const handleResize = () => {
-      setShowBackground(window.innerWidth < 2000);
-    };
-
-    // Définir l'état initial en fonction de la largeur de la fenêtre
+    const handleResize = () => setShowBackground(window.innerWidth < 2000);
     handleResize();
-
-    // Ajouter le gestionnaire d'événements pour le redimensionnement
     window.addEventListener('resize', handleResize);
-
-    // Nettoyer le gestionnaire d'événements lors du démontage
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   useEffect(() => {
     if (isClient) { 
-        // Gestion du blocage du défilement
         const handleScroll = () => {
             if (!showContent || showContent === null) {
                 document.body.style.overflow = 'hidden';
@@ -62,17 +82,12 @@ export default function Home() {
                 document.body.style.overflow = '';
             }
         };
-
-        handleScroll(); // Appel initial pour définir l'état correct du défilement
-
-        // Ajouter les gestionnaires d'événements, etc.
-
+        handleScroll(); 
         return () => {
-            // Nettoyage
-            document.body.style.overflow = ''; // Assurez-vous de réinitialiser le défilement lors du démontage
+            document.body.style.overflow = '';
         };
       }
-  }, [showContent]); // Dépendance sur showContent
+  }, [showContent]); 
 
   // Sélectionnez la clé de langue actuelle de l'état Redux
   type LanguageKey = 'fr' | 'eng';
@@ -99,13 +114,16 @@ export default function Home() {
     localStorage.setItem('showContent', 'false'); // Stockez la réponse dans localStorage
   };
 
-  useEffect(() => {
-    if (image === 2) {
+    const handleImageLoad = () => {
+      setImagesLoaded(prev => prev + 1);
+    };
+
+   useEffect(() => {
+    // Si toutes les images sont chargées, mettez à jour l'état en conséquence
+    if (imagesLoaded >= 2) { // Adaptez ce nombre au nombre d'images que vous attendez
       setShowLoader(false);
-    } else {
-      setShowLoader(true);
     }
-  }, [image]);
+  }, [imagesLoaded]);
 
   return (
     <>
@@ -155,10 +173,10 @@ export default function Home() {
               <div className={`w-full h-screen fixed bg-white md:pt-20 md:py-0 py-20 flex justify-center items-center ${showBackground ? ' backgournTwo' : ''}`} style={{zIndex: "1000"}}>
                 <div className={`mx-auto max-w-7xl py-2 px-4 justify-center items-center min-h-[392px] w-full ${!showLoader ? "opacity-100" : "opacity-0"}`} style={{zIndex: "2000"}}>
                   <div className='flex flex-col items-center'>
-                      <Image src='/logo/logo-hero.png' alt='Château Dutruch Grand Poujeaux' width={300} height={200} onLoadingComplete={()=>{setImage((prev)=>prev + 1)}} />
+                      <img src='/logo/logo-hero.png' alt='Château Dutruch Grand Poujeaux' className="w-[300px] h-auto" onLoad={handleImageLoad} />
                       <div className="mt-10 flex items-end">
                           <div className='w-14 h-0.5 bg-red mb-1'></div>
-                          <Image src='/logo/logo-moulis-medoc.png' alt='moulis-en-medoc' width={150} height={50} className='mr-3 ml-3' onLoadingComplete={()=>{setImage((prev)=>prev + 1)}} />
+                          <img src='/logo/logo-moulis-medoc.png' alt='moulis-en-medoc' className='mr-3 ml-3 w-[150px] h-auto' onLoad={handleImageLoad} />
                           <div className='w-14 h-0.5 bg-red mb-1'></div>
                       </div>
                       <p className="text-text font-cormorant max-w-[450px] font-bold text-lg text-center mt-10">En visitant ce site, je certifie que j’ai l’âge légal pour la consommation d’alcool dans mon pays de résidence.</p>
