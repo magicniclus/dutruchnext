595ec638f9bd0b34b904144f4defe6271b56c198
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-02-06 09:54:34 +0100

    Update Bottles.tsx

--- src/app/components/Bottles.tsx
+++ src/app/components/Bottles.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 import gsap from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
@@ -28,15 +28,22 @@ interface BottlesProps {
 }
 
 const Bottles = ({lang}: BottlesProps) => {
+    
 
     const [bottleIdx, setBottleIdx] = useState<number | null>(null);
 
     const bottleRef = useRef(null);
+    const bottleDescriptionRef = useRef<HTMLDivElement>(null);
 
     const handleBottleInformations = (index: number) => {
-        console.log(index);
         setBottleIdx(index);
     }
+
+    useEffect(() => {
+    if (bottleIdx !== null) {
+        bottleDescriptionRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+}, [bottleIdx]);
 
     const cardBottleOne = () => {
         if (lang === "fr") {
@@ -729,7 +736,7 @@ const Bottles = ({lang}: BottlesProps) => {
                             <div className='mx-auto flex flex-col max-w-5xl py-2 px-4 w-full'>
                                 {
                                     bottleIdx !== null && bottleIdx === index && (
-                                        <div className={`md:hidden relative w-full min-h-[500px] border border-red mx-auto flex max-w-5xl p-5 mt-10 bg-white z-10 transition duration-150 ease-in-out ${bottleIdx !== null ? "opacity-100" : "opacity-0" }`}>
+                                        <div className={`md:hidden relative w-full min-h-[500px] border border-red mx-auto flex max-w-5xl p-5 mt-10 bg-white z-10 transition duration-150 ease-in-out ${bottleIdx !== null ? "opacity-100" : "opacity-0" }`} ref={bottleDescriptionRef}>
                                             <div className={`absolute ${index === bottleIdx ? "h-[50px]" : bottleIdx !== null && bottleIdx !== index ? "" : "h-[50px]"} w-0.5 bg-red -top-[50px] md:hidden flex left-1/2 transform -translate-x-1/2 `}></div>
                                             <div className='absolute p-1 top-3 md:top-5 right-3 md:right-5 rounded-full border border-red cursor-pointer hover:scale-105 transition duration-300 ease-in-out' onClick={()=>{setBottleIdx(null)}}><img src="/icon/close.png" /></div>
                                             {
